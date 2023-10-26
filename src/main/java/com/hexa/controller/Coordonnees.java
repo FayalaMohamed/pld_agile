fd71837f442febc9efcb44c8d57a01fb14b41d30
martinlecluse <martinlecluse76@gmail.com>
2023-10-26 11:17:59 +0200

    coordonnees

---
+++ src/main/java/com/hexa/controller/Coordonnees.java
@@ -0,0 +1,11 @@
+package com.hexa.controller;
+
+public class Coordonnees {
+    
+    private int x;
+    private int y;
+
+    public int getX() { return x;}
+    public int getY() { return y;}
+
+}
