3066f983c059fc80fbd978d4da744fe3edf3ad10
Thomas BOYER <thomasboyer@fedora.home>
2023-11-16 20:55:29 +0100

    commentaires pour javadoc

--- src/main/java/com/hexa/controller/state/EtatCreerRequete3.java
+++ src/main/java/com/hexa/controller/state/EtatCreerRequete3.java
@@ -1,60 +1,68 @@
 package com.hexa.controller.state;
 
-
 import com.hexa.controller.Controller;
 import com.hexa.controller.command.AjoutRequeteApresCircuit;
 import com.hexa.model.*;
 import com.hexa.view.Window;
 
+/**
+ * Etat de l'application quand on se trouve dans la troisième étape de la
+ * création d'une requête correspondant à la sélection de la livraison après
+ * laquelle insérer une nouvelle livraison dans le cas où la tournée est déjà
+ * calculée
+ * --> clickGauche sur une livraison pour choisir la livraison précédant la
+ * nouvelle livraison
+ * --> clicDroit annule la création de requête et revient à etatCarteChargee
+ */
 public class EtatCreerRequete3 implements State {
-	private Livraison livraison;
-	private Tournee tournee;
+  private Livraison livraison;
+  private Tournee tournee;
 
-	/**
-	 * Rentrer dans l'état EtatCreerRequete3 et donner en paramètre l'intersection à
-	 * ajouter à la tournée après calcul du plus court chemin
-	 * 
-	 * @param intersection
-	 */
-	public void entryAction(Livraison livraison, Tournee tournee) {
-		this.livraison = livraison;
-		this.tournee = tournee;
-	}
+  /**
+   * Rentrer dans l'état EtatCreerRequete3 et donner en paramètre l'intersection à
+   * ajouter à la tournée après calcul du plus court chemin
+   * 
+   * @param intersection
+   */
+  public void entryAction(Livraison livraison, Tournee tournee) {
+    this.livraison = livraison;
+    this.tournee = tournee;
+  }
 
-	public void entryAction(Window w) {
-		w.hideButtons(this);
-	}
+  public void entryAction(Window w) {
+    w.hideButtons(this);
+  }
 
-	/**
-	 * Parcourt les intersections de la carte du controlleur et si ça correspond à
-	 * une intersection qui appartient à la tournée (ou à l'entrepôt) passage à
-	 * l'état EtatCreerRequete2
-	 * 
-	 * @param c
-	 * @param w
-	 * @param coordonneesSouris
-	 * @throws TourneeException 
-	 */
-	public void clicGauche(Controller c, Window w, Coordonnees coordonneesSouris) throws TourneeException {
+  /**
+   * Parcourt les intersections de la carte du controlleur et si ça correspond à
+   * une intersection qui appartient à la tournée (ou à l'entrepôt) passage à
+   * l'état EtatCreerRequete2
+   * 
+   * @param c
+   * @param w
+   * @param coordonneesSouris
+   * @throws TourneeException
+   */
+  public void clicGauche(Controller c, Window w, Coordonnees coordonneesSouris) throws TourneeException {
 
-		Intersection intersection = w.getIntersectionsSelectionnees(coordonneesSouris).get(0);
+    Intersection intersection = w.getIntersectionsSelectionnees(coordonneesSouris).get(0);
 
-		if (tournee.estLieuLivraison(intersection) || intersection.equals(c.getCarte().getEntrepot())) {
-			c.getListOfCommands().add(new AjoutRequeteApresCircuit(tournee, c.getCarte(), livraison, intersection));
+    if (tournee.estLieuLivraison(intersection) || intersection.equals(c.getCarte().getEntrepot())) {
+      c.getListOfCommands().add(new AjoutRequeteApresCircuit(tournee, c.getCarte(), livraison, intersection));
 
-			c.switchToState(c.getEtatAuMoinsUneRequete());
-			w.afficherMessage("Livraison Insérée !");
-		}
-	}
+      c.switchToState(c.getEtatAuMoinsUneRequete());
+      w.afficherMessage("Livraison Insérée !");
+    }
+  }
 
-	/**
-	 * Reset le state du controlleur au previousState
-	 * 
-	 * @param c
-	 * @param w
-	 */
-	public void clicDroit(Controller c, Window w) {
-		w.afficherMessage("Création de requête annulée");
-		c.switchToState(c.getPreviousState());
-	}
+  /**
+   * Reset le state du controlleur au previousState
+   * 
+   * @param c
+   * @param w
+   */
+  public void clicDroit(Controller c, Window w) {
+    w.afficherMessage("Création de requête annulée");
+    c.switchToState(c.getPreviousState());
+  }
 }
