aa0968e073d0dd7c63dcecf8ec507d18870ced06
totoletoto69 <thomas.boyer69100@gmail.com>
2023-11-17 08:34:21 +0100

    numeros

--- src/main/java/com/hexa/view/object/VueLivraison.java
+++ src/main/java/com/hexa/view/object/VueLivraison.java
@@ -11,51 +11,51 @@ import com.hexa.view.GraphicalView;
 
 public class VueLivraison extends Observable {
 
-    private GraphicalView gv;
+  private GraphicalView gv;
 
-    private Livraison livraison;
-    private boolean dansTourneeCalculee;
-    private int numero;
+  private Livraison livraison;
+  private boolean dansTourneeCalculee;
+  private int numero;
 
-    private Color color;
-    private int rayon = 6;
-    private Coordonnees coord;
+  private Color color;
+  private int rayon = 6;
+  private Coordonnees coord;
 
-    public VueLivraison(Livraison l, GraphicalView gv, Color color){
-        this.gv = gv;
-        this.livraison = l;
-        this.color = color;
-        this.dansTourneeCalculee = false;
+  public VueLivraison(Livraison l, GraphicalView gv, Color color) {
+    this.gv = gv;
+    this.livraison = l;
+    this.color = color;
+    this.dansTourneeCalculee = false;
+  }
+
+  public VueLivraison(Livraison l, GraphicalView gv, Color color, int numero) {
+    this.gv = gv;
+    this.livraison = l;
+    this.color = color;
+    this.dansTourneeCalculee = true;
+    this.numero = numero;
+  }
+
+  public void dessinerVue() {
+
+    coord = this.gv.CoordGPSToViewPos(livraison.getLieu());
+
+    Graphics g = gv.getGraphics2();
+    g.setColor(color);
+    g.fillOval(coord.getX() - rayon, coord.getY() - rayon, 2 * rayon, 2 * rayon);
+
+    if (dansTourneeCalculee) {
+      g.setColor(Color.black);
+      g.setFont(new Font("TimesRoman", Font.BOLD, (int) (35)));
+      g.drawString(String.valueOf(numero), coord.getX() + rayon, coord.getY() + rayon);
     }
+  }
 
-    public VueLivraison(Livraison l, GraphicalView gv, Color color, int numero){
-        this.gv = gv;
-        this.livraison = l;
-        this.color = color;
-        this.dansTourneeCalculee = true;
-        this.numero = numero;
-    }
+  public boolean estCliquee(Coordonnees coordonneesSouris) {
+    return coord.equals(coordonneesSouris);
+  }
 
-    public void dessinerVue() {
-
-        coord = this.gv.CoordGPSToViewPos(livraison.getLieu());
-
-        Graphics g = gv.getGraphics2();
-        g.setColor(color);
-        g.fillOval(coord.getX() - rayon, coord.getY() - rayon, 2 * rayon, 2 * rayon);
-
-        if (dansTourneeCalculee) {
-            g.setColor(Color.black);
-            g.setFont(new Font("TimesRoman", Font.BOLD, (int) (25 / gv.getZoomFactor() + 1)));
-            g.drawString(String.valueOf(numero), coord.getX() + rayon, coord.getY() + rayon);
-        }
-    }
-
-    public boolean estCliquee(Coordonnees coordonneesSouris) {
-        return coord.equals(coordonneesSouris);
-    }
-
-    public Livraison getLivraison() {
-        return livraison;
-    }
+  public Livraison getLivraison() {
+    return livraison;
+  }
 }
